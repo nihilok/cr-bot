f93e4a6f9522280c187fa13c68f460e2fcf2fc40
Michael Jarvis <mjarvis@ep.com>
2023-11-20 21:06:19 +0000

    Refactor code review output and argument documentation
    The output of the code reviews has been cleaned up by removing redundant messages. In addition, the argument documentation has been expanded for clearer understanding. The code for handling a possible review failure has also been simplified for overall reliability and readability.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let review_comments = ai_funcs::code_review(prompt).await;
         match review_comments {
             Ok(_) => {
-                println!("\n\n== END OF COMMENTS ==");
                 process::exit(0);
             }
             Err(e) => {
@@ -42,7 +41,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         process::exit(1);
     });
 
-    println!("Getting PR data: {}/{} #{}", owner, repo, pr_number);
+    println!("Analysing PR changes: {}/{} #{}\n", owner, repo, pr_number);
     match git_funcs::get_pr_info(&owner, &repo, pr_number).await {
         Ok(pr_info) => {
             let mut output = String::new();
@@ -53,16 +52,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 );
                 utils::append_with_newline(&file.patch, &mut output);
             }
-            println!("Analysing changes...\n");
             let review_result = ai_funcs::code_review(output).await;
-            match review_result {
-                Ok(_) => {
-                    println!("\n\n== END ==");
-                }
-                Err(e) => {
-                    eprintln!("Failed to analyse code: {}", e);
-                    process::exit(1);
-                }
+            if let Err(e) = review_result {
+                eprintln!("Failed to analyze code: {}", e);
+                process::exit(1);
             }
         }
         Err(e) => {
